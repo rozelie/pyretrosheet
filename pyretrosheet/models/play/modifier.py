db07477673fe76d6086b735ad9b991540511eefc
rozelie <ryan.ozelie@gmail.com>
2023-11-30 12:50:31 -0600

    Implement and test play modifiers

--- pyretrosheet/models/play/modifier.py
+++ pyretrosheet/models/play/modifier.py
@@ -1,6 +1,60 @@
 """Encapsulates Retrosheet modifiers as part of play data."""
 
+import re
 from dataclasses import dataclass
+from enum import Enum, auto
+
+from pyretrosheet.models.base import Base
+from pyretrosheet.models.exceptions import ParseError
+
+
+class ModifierType(Enum):
+    """Play modifier type, as defined in Retrosheet spec."""
+
+    APPEAL_PLAY = auto()
+    POP_UP_BUNT = auto()
+    GROUND_BALL_BUNT = auto()
+    BUNT_GROUNDED_INTO_DOUBLE_PLAY = auto()
+    BATTER_INTERFERENCE = auto()
+    LINE_DRIVE_BUNT = auto()
+    BATTING_OUT_OF_TURN = auto()
+    BUNT_POP_UP = auto()
+    BUNT_POPPED_INTO_DOUBLE_PLAY = auto()
+    RUNNER_HIT_BY_BATTED_BALL = auto()
+    CALLED_THIRD_STRIKE = auto()
+    COURTESY_BATTER = auto()
+    COURTESY_FIELDER = auto()
+    COURTESY_RUNNER = auto()
+    UNSPECIFIED_DOUBLE_PLAY = auto()
+    ERROR = auto()
+    FLY = auto()
+    FLY_BALL_DOUBLE_PLAY = auto()
+    FAN_INTERFERENCE = auto()
+    FOUL = auto()
+    FORCE_OUT = auto()
+    GROUND_BALL = auto()
+    GROUND_BALL_DOUBLE_PLAY = auto()
+    GROUND_BALL_TRIPLE_PLAY = auto()
+    INFIELD_FLY_RULE = auto()
+    INTERFERENCE = auto()
+    INSIDE_THE_PARK_HOME_RUN = auto()
+    LINE_DRIVE = auto()
+    LINED_INTO_DOUBLE_PLAY = auto()
+    LINED_INTO_TRIPLE_PLAY = auto()
+    MANAGER_CHALLENGE_OF_CALL_ON_THE_FIELD = auto()
+    NO_DOUBLE_PLAY_CREDITED_FOR_THIS_PLAY = auto()
+    OBSTRUCTION = auto()
+    POP_FLY = auto()
+    RUNNER_PASSED = auto()
+    RELAY_THROW = auto()
+    RUNNER_INTERFERENCE = auto()
+    SACRIFICE_FLY = auto()
+    SACRIFICE_HIT_BUNT = auto()
+    THROW = auto()
+    UNSPECIFIED_TRIPLE_PLAY = auto()
+    UMPIRE_INTERFERENCE = auto()
+    UMPIRE_REVIEW_OF_CALL_ON_THE_FIELD = auto()
+    HIT_LOCATION = auto()
 
 
 @dataclass
@@ -8,9 +62,17 @@ class Modifier:
     """Encodes a play modifier.
 
     Args:
+        type: the type of modifier, as defined in Retrosheet spec
+        hit_location: a hit location, if it exists
+        fielder_position: a fielder position relevant to the modifier, if it exists
+        base: a base relevant to the modifier, if it exists
         raw: the raw play modifier
     """
 
+    type: ModifierType
+    hit_location: str | None
+    fielder_position: int | None
+    base: Base | None
     raw: str
 
     @classmethod
@@ -19,6 +81,120 @@ class Modifier:
 
         Args:
             modifier: a modifier part of a play's event
-                Examples include: @TODO
         """
-        return cls(raw=modifier)
+        modifier_type = _get_modifier_type(modifier)
+        return cls(
+            type=modifier_type,
+            hit_location=_get_hit_location(modifier, modifier_type),
+            fielder_position=_get_fielder_position(modifier, modifier_type),
+            base=_get_base(modifier, modifier_type),
+            raw=modifier,
+        )
+
+
+def _get_modifier_type(modifier: str) -> ModifierType:
+    """Get the modifier type via the raw the modifier.
+
+    Args:
+        modifier: a modifier part of a play's event
+    """
+    # (\d+.*)? matches hit location which is an optional amount of digits followed by an optional amount
+    # of alphabetic characters
+    pattern_to_modifier_type = {
+        r"AP(\d+.*)?": ModifierType.APPEAL_PLAY,
+        r"BP(\d+.*)?": ModifierType.POP_UP_BUNT,
+        r"BG(\d+.*)?": ModifierType.GROUND_BALL_BUNT,
+        r"BGDP(\d+.*)?": ModifierType.BUNT_GROUNDED_INTO_DOUBLE_PLAY,
+        r"BINT(\d+.*)?": ModifierType.BATTER_INTERFERENCE,
+        r"BL(\d+.*)?": ModifierType.LINE_DRIVE_BUNT,
+        r"BOOT(\d+.*)?": ModifierType.BATTING_OUT_OF_TURN,
+        r"BPDP(\d+.*)?": ModifierType.BUNT_POPPED_INTO_DOUBLE_PLAY,
+        r"BR(\d+.*)?": ModifierType.RUNNER_HIT_BY_BATTED_BALL,
+        r"C(\d+.*)?": ModifierType.CALLED_THIRD_STRIKE,
+        r"COUB(\d+.*)?": ModifierType.COURTESY_BATTER,
+        r"COUF(\d+.*)?": ModifierType.COURTESY_FIELDER,
+        r"COUR(\d+.*)?": ModifierType.COURTESY_RUNNER,
+        r"DP(\d+.*)?": ModifierType.UNSPECIFIED_DOUBLE_PLAY,
+        r"E\d(\d+.*)?": ModifierType.ERROR,
+        r"F(\d+.*)?": ModifierType.FLY,
+        r"FDP(\d+.*)?": ModifierType.FLY_BALL_DOUBLE_PLAY,
+        r"FINT(\d+.*)?": ModifierType.FAN_INTERFERENCE,
+        r"FL(\d+.*)?": ModifierType.FOUL,
+        r"FO(\d+.*)?": ModifierType.FORCE_OUT,
+        r"G(\d+.*)?": ModifierType.GROUND_BALL,
+        r"GDP(\d+.*)?": ModifierType.GROUND_BALL_DOUBLE_PLAY,
+        r"GTP(\d+.*)?": ModifierType.GROUND_BALL_TRIPLE_PLAY,
+        r"IF(\d+.*)?": ModifierType.INFIELD_FLY_RULE,
+        r"INT(\d+.*)?": ModifierType.INTERFERENCE,
+        r"IPHR(\d+.*)?": ModifierType.INSIDE_THE_PARK_HOME_RUN,
+        r"L(\d+.*)?": ModifierType.LINE_DRIVE,
+        r"LDP(\d+.*)?": ModifierType.LINED_INTO_DOUBLE_PLAY,
+        r"LTP(\d+.*)?": ModifierType.LINED_INTO_TRIPLE_PLAY,
+        r"MREV(\d+.*)?": ModifierType.MANAGER_CHALLENGE_OF_CALL_ON_THE_FIELD,
+        r"NDP(\d+.*)?": ModifierType.NO_DOUBLE_PLAY_CREDITED_FOR_THIS_PLAY,
+        r"OBS(\d+.*)?": ModifierType.OBSTRUCTION,
+        r"P(\d+.*)?": ModifierType.POP_FLY,
+        r"PASS(\d+.*)?": ModifierType.RUNNER_PASSED,
+        r"R\d(\d+.*)?": ModifierType.RELAY_THROW,
+        r"RINT(\d+.*)?": ModifierType.RUNNER_INTERFERENCE,
+        r"SF(\d+.*)?": ModifierType.SACRIFICE_FLY,
+        r"SH(\d+.*)?": ModifierType.SACRIFICE_HIT_BUNT,
+        r"TH(\d)?(\d+.*)?": ModifierType.THROW,
+        r"TP(\d+.*)?": ModifierType.UNSPECIFIED_TRIPLE_PLAY,
+        r"UINT(\d+.*)?": ModifierType.UMPIRE_INTERFERENCE,
+        r"UREV(\d+.*)?": ModifierType.UMPIRE_REVIEW_OF_CALL_ON_THE_FIELD,
+        r"\d+": ModifierType.HIT_LOCATION,
+    }
+    for pattern, modifier_type in pattern_to_modifier_type.items():
+        if re.fullmatch(pattern, modifier):
+            return modifier_type
+
+    raise ParseError("modifer_type", modifier)
+
+
+def _get_hit_location(modifier: str, modifier_type: ModifierType) -> str | None:
+    """Get the hit location from the modifier, if it exists.
+
+    Args:
+        modifier: a modifier part of a play's event
+        modifier_type: the modifier type
+    """
+    hit_location_re = r"[A-Z]+(\d+.*)"
+    if modifier_type in [ModifierType.ERROR, ModifierType.RELAY_THROW]:
+        hit_location_re = r"[ER]\d(\d+)"
+    elif modifier_type == ModifierType.THROW:
+        hit_location_re = r"TH\d(\d+)"
+    elif modifier_type == ModifierType.HIT_LOCATION:
+        hit_location_re = r"(.*)"
+
+    if match := re.fullmatch(hit_location_re, modifier):
+        return match.group(1)
+
+    return None
+
+
+def _get_fielder_position(modifier: str, modifier_type: ModifierType) -> int | None:
+    """Get the fielder position from the modifier, if it exists.
+
+    Args:
+        modifier: a modifier part of a play's event
+        modifier_type: the type of modifier
+    """
+    if modifier_type in [ModifierType.ERROR, ModifierType.RELAY_THROW]:
+        return int(re.fullmatch(r"[ER](\d)", modifier).group(1))  # type: ignore
+
+    return None
+
+
+def _get_base(modifier: str, modifier_type: ModifierType) -> Base | None:
+    """Get the base from the modifier, if it exists.
+
+    Args:
+        modifier: a modifier part of a play's event
+        modifier_type: the type of modifier
+    """
+    if modifier_type == ModifierType.THROW:  # noqa: SIM102
+        if match := re.fullmatch(r"TH(\d)", modifier):
+            return Base(match.group(1))
+
+    return None
---
+++ tests/unit/models/play/test_modifier.py
@@ -0,0 +1,99 @@
+import pytest
+
+from pyretrosheet.models.base import Base
+from pyretrosheet.models.play import modifier
+from pyretrosheet.models.play.modifier import ModifierType
+
+
+@pytest.mark.parametrize(
+    ["raw_modifier", "expected_modifier_type"],
+    [
+        ("AP", ModifierType.APPEAL_PLAY),
+        ("BP", ModifierType.POP_UP_BUNT),
+        ("BG", ModifierType.GROUND_BALL_BUNT),
+        ("BGDP", ModifierType.BUNT_GROUNDED_INTO_DOUBLE_PLAY),
+        ("BINT", ModifierType.BATTER_INTERFERENCE),
+        ("BL", ModifierType.LINE_DRIVE_BUNT),
+        ("BOOT", ModifierType.BATTING_OUT_OF_TURN),
+        ("BPDP", ModifierType.BUNT_POPPED_INTO_DOUBLE_PLAY),
+        ("BR", ModifierType.RUNNER_HIT_BY_BATTED_BALL),
+        ("C", ModifierType.CALLED_THIRD_STRIKE),
+        ("COUB", ModifierType.COURTESY_BATTER),
+        ("COUF", ModifierType.COURTESY_FIELDER),
+        ("COUR", ModifierType.COURTESY_RUNNER),
+        ("DP", ModifierType.UNSPECIFIED_DOUBLE_PLAY),
+        ("E1", ModifierType.ERROR),
+        ("F", ModifierType.FLY),
+        ("FDP", ModifierType.FLY_BALL_DOUBLE_PLAY),
+        ("FINT", ModifierType.FAN_INTERFERENCE),
+        ("FL", ModifierType.FOUL),
+        ("FO", ModifierType.FORCE_OUT),
+        ("G", ModifierType.GROUND_BALL),
+        ("GDP", ModifierType.GROUND_BALL_DOUBLE_PLAY),
+        ("GTP", ModifierType.GROUND_BALL_TRIPLE_PLAY),
+        ("IF", ModifierType.INFIELD_FLY_RULE),
+        ("INT", ModifierType.INTERFERENCE),
+        ("IPHR", ModifierType.INSIDE_THE_PARK_HOME_RUN),
+        ("L", ModifierType.LINE_DRIVE),
+        ("LDP", ModifierType.LINED_INTO_DOUBLE_PLAY),
+        ("LTP", ModifierType.LINED_INTO_TRIPLE_PLAY),
+        ("MREV", ModifierType.MANAGER_CHALLENGE_OF_CALL_ON_THE_FIELD),
+        ("NDP", ModifierType.NO_DOUBLE_PLAY_CREDITED_FOR_THIS_PLAY),
+        ("OBS", ModifierType.OBSTRUCTION),
+        ("P", ModifierType.POP_FLY),
+        ("PASS", ModifierType.RUNNER_PASSED),
+        ("R1", ModifierType.RELAY_THROW),
+        ("RINT", ModifierType.RUNNER_INTERFERENCE),
+        ("SF", ModifierType.SACRIFICE_FLY),
+        ("SH", ModifierType.SACRIFICE_HIT_BUNT),
+        ("TH", ModifierType.THROW),
+        ("TH1", ModifierType.THROW),
+        ("TP", ModifierType.UNSPECIFIED_TRIPLE_PLAY),
+        ("UINT", ModifierType.UMPIRE_INTERFERENCE),
+        ("UREV", ModifierType.UMPIRE_REVIEW_OF_CALL_ON_THE_FIELD),
+        ("78", ModifierType.HIT_LOCATION),
+    ],
+)
+def test__get_modifier_type(raw_modifier, expected_modifier_type):
+    assert modifier._get_modifier_type(raw_modifier) == expected_modifier_type
+
+
+# L89S
+@pytest.mark.parametrize(
+    ["raw_modifier", "modifier_type", "expected_hit_location"],
+    [
+        ("AP", ModifierType.APPEAL_PLAY, None),
+        ("L89S", ModifierType.LINE_DRIVE, "89S"),
+        ("E1", ModifierType.ERROR, None),
+        ("E12", ModifierType.ERROR, "2"),
+        ("R1", ModifierType.RELAY_THROW, None),
+        ("R12", ModifierType.RELAY_THROW, "2"),
+        ("TH1", ModifierType.THROW, None),
+        ("TH12", ModifierType.THROW, "2"),
+        ("89", ModifierType.HIT_LOCATION, "89"),
+    ],
+)
+def test__get_hit_location(raw_modifier, modifier_type, expected_hit_location):
+    assert modifier._get_hit_location(raw_modifier, modifier_type) == expected_hit_location
+
+
+@pytest.mark.parametrize(
+    ["raw_modifier", "modifier_type", "expected_fielder_position"],
+    [
+        ("E1", ModifierType.ERROR, 1),
+        ("R1", ModifierType.RELAY_THROW, 1),
+    ],
+)
+def test__get_fielder_position(raw_modifier, modifier_type, expected_fielder_position):
+    assert modifier._get_fielder_position(raw_modifier, modifier_type) == expected_fielder_position
+
+
+@pytest.mark.parametrize(
+    ["raw_modifier", "modifier_type", "expected_base"],
+    [
+        ("TH", ModifierType.THROW, None),
+        ("TH1", ModifierType.THROW, Base.FIRST_BASE),
+    ],
+)
+def test__get_base(raw_modifier, modifier_type, expected_base):
+    assert modifier._get_base(raw_modifier, modifier_type) == expected_base
