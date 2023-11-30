3c390983746663b9d6b295214f214674c45f5f46
rozelie <ryan.ozelie@gmail.com>
2023-11-30 16:39:56 -0600

    Consolidate outs into advances due to their similarities
    Retrosheet also treats them mostly the same

--- pyretrosheet/models/play/advance.py
+++ pyretrosheet/models/play/advance.py
@@ -5,26 +5,45 @@ from dataclasses import dataclass
 from pyretrosheet.models.base import Base
 from pyretrosheet.models.exceptions import ParseError
 
+UNEARNED_RUN = "UR"
+RBI_CREDITED = "RBI"
+RBI_NOT_CREDITED = "NORBI"
+TEAM_UNEARNED_RUN = "TUR"
+RESERVED_ADDITIONAL_INFO = [UNEARNED_RUN, RBI_CREDITED, RBI_NOT_CREDITED, TEAM_UNEARNED_RUN]
+
 
 @dataclass
 class Advance:
     """Encodes the advance from one base to another.
 
+    Note this also includes outs.
+
     Args:
         from_base: the base the player is coming from
         to_base: the base the player is advancing to
-        is_unearned_explicit: if a run is explicitly unearned
+        additional_info: some advances encode additional info
+        fielder_assists: map of the fielder positions of players to number of assists, if any
+        fielder_put_out: the fielder position of the player putting the runner out, if any player does a put out
+        fielder_errors: map of the fielder positions of players to number of errors committed, if any
+        fielder_handlers: map of the fielder positions of players to number of times the fielder
+            handled the ball for a play that did not result in a real out, if any
+        is_out: the advance results in the runner being out
+        is_unearned_run_explicit: if a run is explicitly unearned
         is_rbi_credited_explicit: if a run has an explicit rbi credited
         is_rbi_not_credited_explicit: if a run has an explicit rbi not credited
         is_team_unearned_run_explicit: if a run is explicitly a team unearned run
-        additional_info: some advances encode additional info
         raw: the raw advance value
     """
 
     from_base: Base
     to_base: Base
     additional_info: list[str]
-    is_unearned_explicit: bool
+    fielder_assists: list[int]
+    fielder_put_out: int | None
+    fielder_handlers: list[int]
+    fielder_errors: list[int]
+    is_out: bool
+    is_unearned_run_explicit: bool
     is_rbi_credited_explicit: bool
     is_rbi_not_credited_explicit: bool
     is_team_unearned_run_explicit: bool
@@ -36,18 +55,24 @@ class Advance:
 
         Args:
             advance: the advance part of a play's event
-                Examples include: 'B-1', '2-3', '1-2(WP)', '2-H(TUR)', '2-H(E4/TH)(UR)(NR)'
+                Examples include: 'B-1', '2-3', '1-2(WP)', '2-H(TUR)', '2-H(E4/TH)(UR)(NR)', '1X2', '1XH(862)'
         """
         from_base, to_base = _get_bases(advance)
         additional_info = _get_additional_info(advance)
+        is_out = _is_out(advance)
         return cls(
             from_base=from_base,
             to_base=to_base,
             additional_info=_get_additional_info(advance),
-            is_unearned_explicit="UR" in additional_info,
-            is_rbi_credited_explicit="RBI" in additional_info,
-            is_rbi_not_credited_explicit="NORBI" in additional_info,
-            is_team_unearned_run_explicit="TUR" in additional_info,
+            fielder_assists=_get_fielder_assists(additional_info),
+            fielder_put_out=_get_fielder_put_out(additional_info, is_out),
+            fielder_handlers=_get_fielder_handlers(additional_info, is_out),
+            fielder_errors=_get_fielder_errors(additional_info, is_out),
+            is_out=is_out,
+            is_unearned_run_explicit=UNEARNED_RUN in additional_info,
+            is_rbi_credited_explicit=RBI_CREDITED in additional_info,
+            is_rbi_not_credited_explicit=RBI_NOT_CREDITED in additional_info,
+            is_team_unearned_run_explicit=TEAM_UNEARNED_RUN in additional_info,
             raw=advance,
         )
 
@@ -58,7 +83,7 @@ def _get_bases(advance: str) -> tuple[Base, Base]:
     Args:
         advance: the advance description
     """
-    match = re.fullmatch(r"([B123H])-([B123H]).*", advance)
+    match = re.fullmatch(r"([B123H])[-X]([B123H]).*", advance)
     if not match:
         raise ParseError("bases_from_advance", advance)
 
@@ -78,3 +103,106 @@ def _get_additional_info(advance: str) -> list[str]:
         advance: the advance description
     """
     return re.findall(r"\(([^)]+)\)", advance)
+
+
+def _is_out(advance: str) -> bool:
+    """Determine if the runner is out as a result of the advance.
+
+    If there is an error within an out, the out does not occur.
+
+    Retrosheet Spec:
+        The error indicator negates the out.
+
+    Args:
+        advance: the advance description
+    """
+    is_out_encoded = bool(re.fullmatch(r"([B123H])X([B123H]).*", advance))
+    has_error = bool(re.search(r"\(.*E.*\)", advance))
+    return is_out_encoded and not has_error
+
+
+def _get_fielder_assists(additional_info: list[str]) -> list[int]:
+    """Get fielder position numbers of fielders with an assist.
+
+    Note that fielders are still given an assist even if an error follows them and an actual out does not occur,
+
+    Args:
+        additional_info: advance additional info
+    """
+    fielder_assists = []
+    for info in additional_info:
+        if info in RESERVED_ADDITIONAL_INFO:
+            continue
+
+        for i, fielder_position in enumerate(info):
+            if fielder_position == "E" or info[i - 1] == "E" or i == len(info) - 1:
+                continue
+
+            fielder_assists.append(int(fielder_position))
+
+    return fielder_assists
+
+
+def _get_fielder_put_out(additional_info: list[str], is_out: bool) -> int | None:
+    """Get the fielder position putting the runner out, if they exist.
+
+    Args:
+        additional_info: advance additional info
+        is_out: if an out occurs
+    """
+    if not is_out:
+        return None
+
+    for info in additional_info:
+        if info in RESERVED_ADDITIONAL_INFO:
+            continue
+
+        return int(info[-1])
+
+    return None
+
+
+def _get_fielder_handlers(additional_info: list[str], is_out: bool) -> list[int]:
+    """Get fielder position numbers of fielders handling the ball in the case of not an actual out occurring.
+
+    Args:
+        additional_info: advance additional info
+        is_out: if an out occurs
+    """
+    if is_out:
+        return []
+
+    fielder_handlers = []
+    for info in additional_info:
+        if info in RESERVED_ADDITIONAL_INFO:
+            continue
+
+        for i, fielder_position in enumerate(info):
+            if fielder_position == "E" or info[i - 1] == "E":
+                continue
+
+            fielder_handlers.append(int(fielder_position))
+
+    return fielder_handlers
+
+
+def _get_fielder_errors(additional_info: list[str], is_out: bool) -> list[int]:
+    """Get fielder position numbers of fielders committing an error on the out.
+
+    Args:
+        additional_info: advance additional info
+        is_out: if an out occurs
+    """
+    if is_out:
+        return []
+
+    fielder_errors = []
+    for info in additional_info:
+        if info in RESERVED_ADDITIONAL_INFO:
+            continue
+
+        for i, fielder_position in enumerate(info):
+            if fielder_position == "E":
+                fielder_errors.append(int(info[i + 1]))
+
+    return fielder_errors
--- tests/unit/models/play/test_advance.py
+++ tests/unit/models/play/test_advance.py
@@ -9,6 +9,9 @@ from pyretrosheet.models.play import advance
     [
         ("B-1", Base.BATTER_AT_HOME, Base.FIRST_BASE),
         ("2-H", Base.SECOND_BASE, Base.HOME),
+        ("BX1", Base.BATTER_AT_HOME, Base.FIRST_BASE),
+        ("2XH", Base.SECOND_BASE, Base.HOME),
+        ("3XH(1E2)", Base.THIRD_BASE, Base.HOME),
     ],
 )
 def test__get_bases(raw_advance, expected_from_base, expected_to_base):
@@ -28,13 +31,82 @@ def test__get_additional_info(raw_advance, expected_additional_info):
 
 
 @pytest.mark.parametrize(
+    ["raw_advance", "expected_is_out"],
+    [
+        ("1-2", False),
+        ("1-2(E3)", False),
+        ("1X2", True),
+        ("1X2(1)", True),
+        ("BX2(7E4)", False),
+    ],
+)
+def test__is_out(raw_advance, expected_is_out):
+    assert advance._is_out(raw_advance) == expected_is_out
+
+
+@pytest.mark.parametrize(
+    ["additional_info", "expected_fielder_assists"],
+    [
+        ([], []),
+        (["1"], []),
+        (["7E4"], [7]),
+        (["13"], [1]),
+        (["123"], [1, 2]),
+    ],
+)
+def test__get_fielder_assists(additional_info, expected_fielder_assists):
+    assert advance._get_fielder_assists(additional_info) == expected_fielder_assists
+
+
+@pytest.mark.parametrize(
+    ["additional_info", "is_out", "expected_fielder_put_out"],
+    [
+        ([], True, None),
+        (["7E4)"], False, None),
+        (["1"], True, 1),
+        (["13"], True, 3),
+    ],
+)
+def test__get_fielder_put_out(additional_info, is_out, expected_fielder_put_out):
+    assert advance._get_fielder_put_out(additional_info, is_out) == expected_fielder_put_out
+
+
+@pytest.mark.parametrize(
+    ["additional_info", "is_out", "expected_fielder_handlers"],
+    [
+        ([], True, []),
+        (["1"], True, []),
+        (["7E4"], False, [7]),
+        (["27E4"], False, [2, 7]),
+        (["27E45"], False, [2, 7, 5]),
+    ],
+)
+def test__get_fielder_handlers(additional_info, is_out, expected_fielder_handlers):
+    assert advance._get_fielder_handlers(additional_info, is_out) == expected_fielder_handlers
+
+
+@pytest.mark.parametrize(
+    ["additional_info", "is_out", "expected_fielder_errors"],
+    [
+        ({"7E4"}, False, [4]),
+        ({"27E4"}, False, [4]),
+        ({"27E4E5"}, False, [4, 5]),
+        ([], True, []),
+        ({"1"}, True, []),
+    ],
+)
+def test__get_fielder_errors(additional_info, is_out, expected_fielder_errors):
+    assert advance._get_fielder_errors(additional_info, is_out) == expected_fielder_errors
+
+
+@pytest.mark.parametrize(
     ["raw_advance", "explicit_attr", "is_explicit"],
     [
-        ("B-1", "is_unearned_explicit", False),
+        ("B-1", "is_unearned_run_explicit", False),
         ("B-1", "is_rbi_credited_explicit", False),
         ("B-1", "is_rbi_not_credited_explicit", False),
         ("B-1", "is_team_unearned_run_explicit", False),
-        ("1-H(UR)", "is_unearned_explicit", True),
+        ("1-H(UR)", "is_unearned_run_explicit", True),
         ("1-H(RBI)", "is_rbi_credited_explicit", True),
         ("1-H(NORBI)", "is_rbi_not_credited_explicit", True),
         ("1-H(TUR)", "is_team_unearned_run_explicit", True),
