0b2b945a74d1eeff4cd737009517d0b7d39c5920
rozelie <ryan.ozelie@gmail.com>
2023-12-01 16:15:22 -0600

    Remove debug comments from test_all_data.py

--- tests/integration/test_all_data.py
+++ tests/integration/test_all_data.py
@@ -8,20 +8,11 @@ import pyretrosheet
 @pytest.mark.skipif(not os.getenv("PYRETROSHEET_TEST_ALL_DATA"), reason="I/O intensive and should only be run locally")
 def test_all_data(tmp_path):
     """Loads all Retrosheet data and confirms that all data is at the very least loadable."""
-    # data_dir = tmp_path / "data"
-    # data_dir.mkdir()
-    #
-    # for year in range(1919, 2023):
-    #     print(f"Testing {year=}...")
-    #     list(pyretrosheet.load_games(year, data_dir))
-    #
-    # # passes if no exception raised
+    data_dir = tmp_path / "data"
+    data_dir.mkdir()
 
-    import pyretrosheet
+    for year in range(1919, 2023):
+        print(f"Testing {year=}...")
+        list(pyretrosheet.load_games(year, data_dir))
 
-    # generator for loading games in a given year
-    games = pyretrosheet.load_games(year=2022)
-
-    for game in games:
-        print(game)  #
-        break
+    # passes if no exception raised
