bc30dec8c04ec85af10ce23e479e0b4a72d23c8a
rozelie <ryan.ozelie@gmail.com>
2024-02-01 12:47:40 -0600

    Refactor test_all_data_loading to use pytest parametrize

--- tests/integration/test_all_data_loading.py
+++ tests/integration/test_all_data_loading.py
@@ -3,21 +3,18 @@ import os
 import pytest
 
 import pyretrosheet
-from pyretrosheet.models.play.modifier import ModifierType
-from pyretrosheet.views import get_plays
+
+START_YEAR = 1919
+END_YEAR = 2023
 
 
 @pytest.mark.skipif(not os.getenv("PYRETROSHEET_TEST_ALL_DATA"), reason="I/O intensive and should only be run locally")
-def test_all_data_loading(tmp_path):
+@pytest.mark.parametrize(("year"), list(range(START_YEAR, END_YEAR + 1)))
+def test_all_data_loading(tmp_path, year):
     """Loads all Retrosheet data and confirms that all data is at the very least loadable."""
     data_dir = tmp_path / "data"
     data_dir.mkdir()
-    start_year = 1919
-    end_year = 2022
 
-    print(f"\nTesting all data from {start_year=} to {end_year=}")
-    for year in range(start_year, end_year + 1):
-        print(f"Testing {year=}...")
-        _ = list(pyretrosheet.load_games(year, data_dir))
+    _ = list(pyretrosheet.load_games(year, data_dir))
 
     # passes if no exception raised
